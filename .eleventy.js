fbfd70983339b7fd60e77b83e0d63fbe9e04d6f5
Paul Prestidge <paul@storypark.com>
2021-06-09 15:46:11 +1200

    Change favicon pass-through and date format

--- .eleventy.js
+++ .eleventy.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy('css');
+  eleventyConfig.addPassthroughCopy({ public: '/' });
 
   return {
     passthroughFileCopy: true,
