0de6d85dd669368d627688442fc2cf2ff5d1f954
Paul Prestidge <paul@storypark.com>
2021-09-10 15:20:03 +1200

    Close off cycle 4.  Nice work team :tada:

--- _data/history.js
+++ _data/history.js
@@ -14,5 +14,20 @@ module.exports = [
       { user: 'zugolosian', reviewCount: 4, prCount: 0 },
       { user: 'BecsController', reviewCount: 1, prCount: 0 },
     ],
+  },
+  {
+    year: 2021,
+    cycle: 4,
+    startDate: '2021-08-02',
+    endDate: '2021-09-10',
+    scores: [
+      { user: 'chron', reviewCount: 34, prCount: 21 },
+      { user: 'timothyjohn2015', reviewCount: 30, prCount: 16 },
+      { user: 'willsmart', reviewCount: 27, prCount: 12 },
+      { user: 'libbyschuknight', reviewCount: 24, prCount: 9 },
+      { user: 'marielleaurabelle', reviewCount: 19, prCount: 11 },
+      { user: 'fourseven', reviewCount: 3, prCount: 1 },
+      { user: 'AlexQuinlivan', reviewCount: 1, prCount: 1 },
+    ],
   }
 ]
