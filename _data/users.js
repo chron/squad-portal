c37ed060fb4a26625a95894f55578793e0816736
Paul Prestidge <paul@storypark.com>
2021-10-07 15:05:56 +1300

    Add the ECD emotion lists to the member pages (Tim's suggestion)

--- _data/users.js
+++ _data/users.js
@@ -3,6 +3,10 @@ module.exports = [
     githubLogin: 'libbyschuknight',
     nickname: 'Libby',
     current: true,
+    emotions: {
+      positive: ['supported', 'connected', 'empathetic', 'non-judgemental', 'secure'],
+      negative: ['embarrassed', 'disheartened', 'anxious', 'incapable', 'judged'],
+    },
   },
   {
     githubLogin: 'louisa-storypark',
@@ -13,21 +17,37 @@ module.exports = [
     nickname: 'Marielle',
     tagline: 'The queen of emoji reactions <em>👸🏻</em>',
     current: true,
+    emotions: {
+      positive: ['connected', 'supported', 'appreciated', 'empathetic', 'helpful'],
+      negative: ['overwhelmed', 'incapable', 'intimidated', 'disheartened'],
+    },
   },
   {
     githubLogin: 'chron',
     nickname: 'Paul',
     current: true,
+    emotions: {
+      positive: ['appreciated', 'playful', 'curious', 'empathetic', 'humble'],
+      negative: ['powerless', 'stuck', 'overwhelmed', 'restless', 'neglect'],
+    },
   },
   {
     githubLogin: 'timothyjohn2015',
     nickname: 'Tim',
     current: true,
+    emotions: {
+      positive: ['connected', 'supported', 'understanding', 'encouraged', 'appreciated'],
+      negative: ['alienated', 'disconnected', 'controlled', 'judged', 'unwelcome'],
+    },
   },
   {
     githubLogin: 'willsmart',
     nickname: 'Will',
     current: true,
+    emotions: {
+      positive: ['joy', 'appreciated', 'curious', 'optimistic', 'brave'],
+      negative: ['uncaring', 'guarded', 'overwhelmed', 'powerless', 'paralysed'],
+    },
   },
   {
     githubLogin: 'BecsController',
