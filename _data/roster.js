bbaf81ac69fca2c6405eb3aa3d068fc58bb5e456
Paul Prestidge <paul@storypark.com>
2022-01-18 15:00:29 +1300

    Roster proposal 2

--- _data/roster.js
+++ _data/roster.js
@@ -1,14 +1,26 @@
 module.exports = [
   {
+    title: 'Cooldown Week 1',
+    dates: 'Jan 25 — Jan 28',
+    main: 'sandemchad',
+    backup: 'timothyjohn2015',
+  },
+  {
+    title: 'Cooldown Week 2',
+    dates: 'Jan 31 — Feb 4',
+    main: 'sandemchad',
+    backup: 'timothyjohn2015',
+  },
+  {
     title: 'Cycle week 1',
     dates: 'Feb 8 — Feb 11',
-    main: 'sandemchad',
+    main: 'timothyjohn2015',
     backup: 'chron',
   },
   {
     title: 'Cycle week 2',
     dates: 'Feb 14 — Feb 18',
-    main: 'sandemchad',
+    main: 'timothyjohn2015',
     backup: 'chron',
   },
   {
@@ -27,25 +39,25 @@ module.exports = [
     title: 'Cycle week 5',
     dates: 'Mar 7 — Mar 11',
     main: 'libbyschuknight',
-    backup: 'timothyjohn2015',
+    backup: 'sandemchad',
   },
   {
     title: 'Cycle week 6',
     dates: 'Mar 14 — Mar 18',
     main: 'libbyschuknight',
-    backup: 'timothyjohn2015',
+    backup: 'sandemchad',
   },
   {
     title: 'Cooldown week 1',
     dates: 'Mar 21 — Mar 25',
-    main: 'timothyjohn2015',
-    backup: 'sandemchad',
+    main: 'sandemchad',
+    backup: 'timothyjohn2015',
   },
   {
     title: 'Cooldown week 2',
     dates: 'Mar 28 — Apr 1',
-    main: 'timothyjohn2015',
-    backup: 'sandemchad',
+    main: 'sandemchad',
+    backup: 'timothyjohn2015',
   },
 
 ]
